4d27ce1ec61d75e30ded1bba3ad9a88614105e3e
superbluesea <349200964@qq.com>
2017-08-30 15:05:37 +0800

    啊

--- Source.cpp
+++ Source.cpp
@@ -1695,9 +1695,27 @@ void Pat1044()
 
 }
 
+//主元为0时仍然输出一行回车
 void Pat1045()
 {
-
+	int N, ary[100000], minAry[100000], maxAry[100000], count = 0;
+	scanf("%d", &N);
+	for (int i = 0; i < N; ++i)scanf("%d", ary + i);
+	maxAry[0] = ary[0];
+	for (int i = 1; i < N; ++i)if (ary[i] >= maxAry[i - 1])maxAry[i] = ary[i]; else maxAry[i] = maxAry[i - 1];
+	minAry[N - 1] = ary[N - 1];
+	for (int i = N - 2; i >= 0; --i)if (ary[i] <= minAry[i + 1])minAry[i] = ary[i]; else minAry[i] = minAry[i + 1];
+	for (int i = 0; i < N; ++i)if (minAry[i] == maxAry[i])++count;
+	printf("%d\n", count);
+	if (count == 0)printf("\n");
+	for (int i = 0; i < N; ++i)
+	{
+		if (minAry[i] == maxAry[i])
+		{
+			--count;
+			if (count)printf("%d ", ary[i]); else printf("%d", ary[i]);
+		}
+	}
 }
 
 void Pat1046()
@@ -2568,6 +2586,8 @@ void Pat1070()
 	}
 	printf("%d\n", (int)d);
 }
+
+
 
 int main()
 {	
